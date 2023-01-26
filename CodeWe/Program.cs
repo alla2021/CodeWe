40fc6474ee96e574f13f694f71236b15beb8a997
Alla Honcharova <alla7gon@gmail.com>
2023-01-26 14:54:21 +0200

    added task2 converting unix timestamp to date

--- CodeWe/Program.cs
+++ CodeWe/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using GeneratorPass;
-using BaseArray;
-using BaseStudent;
-using BasePerson;
+using Task2_ConvertTime;
 
 namespace task
 {
@@ -12,51 +7,8 @@ namespace task
     {
         static void Main(string[] args)
         {
-            //array
-            /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-             Git Revert
-            Git Revert
-            Git Revert
-            Git Revert
-            Git Revert
-            Git Revertkkkkkkkkkkkk
-            Git Revertkkkkkkkkkk
-            Git Revertkkkkkkkkkk
-            Git Revertadadadaadadad
-             */
-
-=======
->>>>>>> parent of f1bb653 (git-revert-test)
-=======
->>>>>>> parent of f1bb653 (git-revert-test)
-            ArrHelper helper = new ArrHelper();
-            helper.createList(5, 6, 10, 30);
-            helper.printList();
-            helper.getSize();
-            helper.getMax();
-            helper.getMin();
-            helper.find(12);
-<<<<<<< HEAD
-<<<<<<< HEAD
-            */
-
-=======
-            
-            //changes11111111111111
->>>>>>> 9fb0330 (test)
-=======
-            
-            //changes11111111111111
->>>>>>> 9fb033023ab1e2f4251030895e5028d485e713f4
-
-            //Password Generator
-            //PasswordGenerator pass = new PasswordGenerator();
-            //Console.WriteLine(pass.passwordGeneratorFunc(4));
-
-            Person obj = new Student("Harry", "c#");
-            obj.PrintInfo();
+           ConvertTime time = new ConvertTime();
+           Console.WriteLine(time.convert(1553355663675));//23 березня 2019 г., 17:41:03
         }
     }
 }
---
+++ CodeWe/task25.01_timeConverter/ConvertTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Task2_ConvertTime
+{
+    public class ConvertTime
+    {
+        private long unixTime;
+
+        public ConvertTime() { }
+
+        public long UnixTime
+        {
+            get => this.unixTime;
+            set => this.unixTime = value;
+        }
+
+        public DateTime convert(long unixTime)
+        {
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+            dt = dt.AddMilliseconds(unixTime).ToLocalTime();
+            string dateString = dt.ToString("MM/dd/yyyy HH:mm", CultureInfo.InvariantCulture);
+            DateTime dateTime = DateTime.Parse(dateString, CultureInfo.InvariantCulture);
+            return dateTime;
+        }
+
+    }
+}
