5d79094e02f009ba797291fffcd341cc4861e553
Alla Honcharova <alla7gon@gmail.com>
2023-01-27 19:07:13 +0200

    task1 calc age

---
+++ CodeWe/CalculateAge.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CalculateUserAge
+{
+    public class CalculateAge 
+    {
+        protected int birthYear;
+        protected int birthDay;
+        protected int birthMonth;
+
+        DateTime userBirthDate;
+        DateTime now;
+
+        protected bool isAdult;
+        protected int adultYear;
+        public CalculateAge(){ }
+
+        public int BirthYear
+        {
+            get => this.birthYear;
+            set => this.birthYear = value;
+        }
+
+        public int BirthDay
+        {
+            get => this.birthDay;
+            set => this.birthDay = value;
+        }
+
+        public int BirthMonth
+        {
+            get => this.birthMonth;
+            set => this.birthMonth = value;
+        }
+
+        public void inputInfo()
+        {
+            try
+            {
+                Console.WriteLine("Enter your --year-- of birth: ");
+                BirthYear = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Enter your --Day-- of birth: ");
+                BirthDay = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter your --Month-- of birth: ");
+                BirthMonth = Convert.ToInt32(Console.ReadLine());
+
+                userBirthDate = birthDate();
+            }
+           catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private DateTime birthDate()
+        {  
+            DateTime theDate = new DateTime(birthYear, birthMonth, birthDay);          
+            return theDate;
+        }
+
+        public int calculateUserAge()
+        {
+            now = DateTime.Now;
+            int age = now.Year - userBirthDate.Year;
+            if (now<userBirthDate) {
+                throw new Exception("ERROR. The birthday cannot be in the future!");
+            } else if (now.Month < userBirthDate.Month || (now.Month == userBirthDate.Month && now.Day < userBirthDate.Day)){ 
+                age--; 
+            }
+            isAdult = age >= 18 ? true : false;
+            if (isAdult)
+            {
+                adultYear = isAdultCalc();
+            }
+            return age;
+        }
+
+        private int isAdultCalc()
+        {
+            DateTime year = userBirthDate.AddYears(18);
+            return year.Year;
+        }
+
+        private int totalDays()
+        {
+            now = DateTime.Now;
+            TimeSpan ts =  now.Subtract(userBirthDate);
+            return Convert.ToInt32(ts.TotalDays);
+        }
+
+        private int totalMonth()
+        {
+            int total = 0;
+
+    
+                total = ((now.Year - userBirthDate.Year) * 12) + now.Month - userBirthDate.Month;   
+            
+            return total;
+        }
+       
+
+        public void printInfo()
+        {  
+            Console.WriteLine($"The person is {calculateUserAge()} years old.");
+            Console.WriteLine(isAdult ? $"The person is ADULT. 18 years was in {adultYear}" : "The person is young.");
+            Console.WriteLine($"You have been alive for {totalDays()} days");
+            Console.WriteLine($"You have been alive for {totalMonth()} months");
+        }
+    }
+}
--- CodeWe/Program.cs
+++ CodeWe/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using GeneratorPass;
-using BaseArray;
-using BaseStudent;
-using BasePerson;
+﻿using CalculateUserAge;
+using System;
 
 namespace task
 {
@@ -12,53 +7,17 @@ namespace task
     {
         static void Main(string[] args)
         {
-            //array
-            /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-             Git Revert
-            Git Revert
-            Git Revert
-            Git Revert
-            Git Revert
-            Git Revertkkkkkkkkkkkk
-            Git Revertkkkkkkkkkk
-            Git Revertkkkkkkkkkk
-            Git Revertadadadaadadad
-             */
+            CalculateAge user1 = new CalculateAge();
+           try
+            {
+                user1.inputInfo();
+                user1.printInfo();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }   
 
-=======
->>>>>>> parent of f1bb653 (git-revert-test)
-=======
->>>>>>> parent of f1bb653 (git-revert-test)
-            ArrHelper helper = new ArrHelper();
-            helper.createList(5, 6, 10, 30);
-            helper.printList();
-            helper.getSize();
-            helper.getMax();
-            helper.getMin();
-            helper.find(12);
-<<<<<<< HEAD
-<<<<<<< HEAD
-            */
-
-=======
-            
-            //changes11111111111111
->>>>>>> 9fb0330 (test)
-=======
-            
-            //changes11111111111111
->>>>>>> 9fb033023ab1e2f4251030895e5028d485e713f4
-
-            //Password Generator
-            //PasswordGenerator pass = new PasswordGenerator();
-            //Console.WriteLine(pass.passwordGeneratorFunc(4));
-
-            Person obj = new Student("Harry", "c#");
-            obj.PrintInfo();
         }
     }
 }
-
-
